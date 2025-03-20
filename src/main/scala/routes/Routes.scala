452ecee9df70963de946c42ec3aea915c329d3ff
Serhii Postument <serhii.postument@patrianna.com>
2025-03-20 19:51:53 +0200

    init

--- src/main/scala/routes/Routes.scala
+++ src/main/scala/routes/Routes.scala
@@ -4,11 +4,12 @@ package routes
 import org.apache.pekko.http.scaladsl.model._
 import org.apache.pekko.http.scaladsl.server.Directives._
 import org.apache.pekko.http.scaladsl.server.Route
-import utils.CloudFlareApi
-import io.circe.syntax._
+import utils.{CloudFlareGraphiQl, CloudFlareApi}
 import scala.util.{Success, Failure}
 import scala.concurrent.ExecutionContext
 import com.typesafe.scalalogging.LazyLogging
+import io.circe.generic.auto._
+import io.circe.syntax._
 
 object Routes extends LazyLogging {
   implicit val ec: ExecutionContext = scala.concurrent.ExecutionContext.global
@@ -31,12 +32,23 @@ object Routes extends LazyLogging {
           val startTime = "2025-03-19T16:00:00Z"
           val endTime = "2025-03-19T17:00:00Z"
 
-          onComplete(CloudFlareApi.fetchFirewallEvents(zoneId, startTime, endTime)) {
+          onComplete(CloudFlareGraphiQl.fetchFirewallEvents(zoneId, startTime, endTime)) {
             case Success(json) =>
               complete(HttpEntity(ContentTypes.`application/json`, json.spaces2))
             case Failure(exception) =>
               logger.error("Failed to fetch firewall events", exception)
               complete(HttpResponse(StatusCodes.InternalServerError, entity = "Error fetching firewall events"))
+          }
+        }
+      },
+      path("zones") {
+        get {
+          onComplete(CloudFlareApi.getZones()) {
+            case Success(zones) =>
+              complete(HttpEntity(ContentTypes.`application/json`, zones.asJson.noSpaces))
+            case Failure(exception) =>
+              logger.error("Failed to fetch zones", exception)
+              complete(HttpResponse(StatusCodes.InternalServerError, entity = "Error fetching zones"))
           }
         }
       }
--- src/main/scala/utils/CloudFlareApi.scala
+++ src/main/scala/utils/CloudFlareApi.scala
@@ -9,65 +9,68 @@ import scala.concurrent.{Future, ExecutionContext}
 import com.typesafe.scalalogging.LazyLogging
 
 object CloudFlareApi extends LazyLogging {
-
   Config.validate()
 
   implicit val backend: SttpBackend[Identity, Any] = HttpURLConnectionBackend()
 
-  def fetchFirewallEvents(zoneId: String, startTime: String, endTime: String)
-                         (implicit ec: ExecutionContext): Future[Json] = {
-    val query =
-      s"""
-         |{
-         |  viewer {
-         |    zones(filter: {zoneTag: "$zoneId"}) {
-         |      topIPs: firewallEventsAdaptiveGroups(
-         |        limit: 100,
-         |        filter:{
-         |          datetime_geq: "$startTime",
-         |          datetime_leq: "$endTime"
-         |        },
-         |        orderBy: [count_DESC]
-         |      ) {
-         |        count,
-         |        avg { sampleInterval }
-         |        dimensions {
-         |          action
-         |          ruleId
-         |          source
-         |        }
-         |      }
-         |    }
-         |  }
-         |}
-         |""".stripMargin
+  def getZones()(implicit ec: ExecutionContext): Future[List[Map[String, String]]] = {
+    val baseUri = uri"https://api.cloudflare.com/client/v4/zones"
 
-    val request = basicRequest
-      .post(uri"${Config.graphqlEndpoint}")
-      .header("Authorization", s"Bearer ${Config.apiToken}")
-      .header("X-AUTH-EMAIL", Config.authEmail)
-      .header("Content-Type", "application/json")
-      .body(Map("query" -> query).asJson.noSpaces)
-      .response(asString)
+    def fetchPage(page: Int): Future[List[Map[String, String]]] = {
+      val request = basicRequest
+        .get(baseUri.addParam("page", page.toString).addParam("per_page", "50"))
+        .header("Authorization", s"Bearer ${Config.apiToken}")
+        .header("X-Auth-Email", Config.authEmail)
+        .header("Content-Type", "application/json")
+        .response(asString)
 
-    logger.info(s"Sending GraphQL request to Cloudflare for zone: $zoneId")
+      logger.info(s"Fetching Cloudflare zones, page: $page")
 
-    Future {
-      val response = request.send(backend)
-      response.body match {
-        case Right(jsonStr) =>
-          parse(jsonStr) match {
-            case Right(json) =>
-              logger.info("Successfully retrieved firewall events")
-              json
-            case Left(parseError) =>
-              logger.error("Failed to parse Cloudflare GraphQL response", parseError)
-              throw parseError
-          }
-        case Left(error) =>
-          logger.error(s"Cloudflare API request failed: $error")
-          throw new Exception(error)
-      }
+      Future {
+        val response = request.send(backend)
+        response.body match {
+          case Right(jsonStr) =>
+            logger.info(s"Cloudflare API Response: $jsonStr")  // LOG THE RESPONSE
+            parse(jsonStr) match {
+              case Right(json) =>
+                val cursor = json.hcursor
+                val zones = cursor.downField("result").as[List[Map[String, Json]]].getOrElse(List())
+
+                val activeZones = zones.collect {
+                  case zone if zone.get("status").contains(Json.fromString("active")) =>
+                    val accountId = zone.get("account").flatMap(_.hcursor.get[String]("id").toOption).getOrElse("")
+                    val accountName = zone.get("account").flatMap(_.hcursor.get[String]("name").toOption).getOrElse("")
+                    val zoneId = zone.get("id").flatMap(_.asString).getOrElse("")
+                    val zoneName = zone.get("name").flatMap(_.asString).getOrElse("")
+                    Map(
+                      "accountId" -> accountId,
+                      "accountName" -> accountName,
+                      "zoneId" -> zoneId,
+                      "zoneName" -> zoneName
+                    )
+                }
+
+                logger.info(s"Active Zones Extracted: ${activeZones.mkString(", ")}")
+
+                val totalPages = cursor.downField("result_info").downField("total_pages").as[Int].getOrElse(1)
+                if (page < totalPages) {
+                  fetchPage(page + 1).map(nextPage => activeZones ++ nextPage)
+                } else {
+                  Future.successful(activeZones)
+                }
+
+              case Left(parseError) =>
+                logger.error("Failed to parse Cloudflare API response", parseError)
+                throw parseError
+            }
+          case Left(error) =>
+            logger.error(s"Cloudflare API request failed: $error")
+            throw new Exception(error)
+        }
+      }.flatten
     }
+
+    fetchPage(1)
   }
+
 }
